0f748bfcd375443ecc53ce640e87044b4799cd10
Robert Beachill <bbeachill@icloud.com>
2025-05-01 23:57:03 +0100

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Robert Beachill on 01/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
