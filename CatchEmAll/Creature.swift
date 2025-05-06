87d47b029a5ca61a3e845f74e516649e414747a6
Robert Beachill <bbeachill@icloud.com>
2025-05-06 19:36:15 +0100

    Finished DetailView interface. Set up Navigation link to pass through name from Creature struct. Put Creature struct definition in separate file so it has global scope

---
+++ CatchEmAll/Creature.swift
@@ -0,0 +1,12 @@
+//
+//  Creature.swift
+//  CatchEmAll
+//
+//  Created by Robert Beachill on 06/05/2025.
+//
+
+import Foundation
+struct Creature: Codable, Hashable {
+    var name: String
+    var url: String // url for detail on Pokemon
+}
--- CatchEmAll/DetailView.swift
+++ CatchEmAll/DetailView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct DetailView: View {
+    let creature: Creature
+    
     var body: some View {
-        VStack(alignment:.leading) {
-        Text("PokeName")
+        VStack(alignment:.leading, spacing: 3) {
+        Text(creature.name)
              .font(Font.custom("Avenir Next Condensed", size: 60))
              .bold()
              .minimumScaleFactor(0.5)
@@ -29,13 +31,44 @@ struct DetailView: View {
                     .frame(width: 96, height: 96)
                     .clipShape(RoundedRectangle(cornerRadius: 16))
                     .shadow(radius: 8, x: 5, y: 5)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.gray.opacity(0.5), lineWidth: 1)
+                    }
+                    .padding(.trailing)
+                
+                VStack {
+                    HStack(alignment:.top) {
+                        Text("Height:")
+                            .font(.title2)
+                            .bold()
+                            .foregroundStyle(.red)
+                        
+                        Text("999.9")
+                            .font(.largeTitle)
+                            .bold()
+                    }
+                    
+                    HStack(alignment:.top) {
+                        Text("Weight:")
+                            .font(.title2)
+                            .bold()
+                            .foregroundStyle(.red)
+                        
+                        Text("999.9")
+                            .font(.largeTitle)
+                            .bold()
+                    }
+                }
             }
             
          Spacer()
+            
         }
+        .padding()
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(creature: Creature(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/"))
 }
